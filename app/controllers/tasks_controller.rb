2e57cf4ca695f148e4f242a6e20db824b93991ba
Elena-Learning <elena.urban.professionell@gmail.com>
2019-11-12 17:05:06 +0100

    almost done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,16 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    raise
+    @task.update
+  end
+
   private
 
   def task_params
